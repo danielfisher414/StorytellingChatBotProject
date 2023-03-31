bb3f9dd53501c49d29b6165131ca02a1be69c169
Daniel <47793260+danielfisher414@users.noreply.github.com>
2023-03-31 02:55:24 +0100

    added video to the background

--- front-end/storytellingApp/App.js
+++ front-end/storytellingApp/App.js
@@ -1,14 +1,43 @@
-import React, { useState,Component } from 'react';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View, FlatList  } from 'react-native';
+import React, { useState,Component,useEffect,useRef } from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View, FlatList   } from 'react-native';
+// import {Video} from 'expo-av';
+// import { requireNativeComponent } from 'react-native';
+// import { Platform } from 'react-native';
+// import video from './assets/videos/9878-background-full-screen.mp4'
+// import { createElement } from 'react-native-web';
 // import { Image } from 'react-native-web';
-// import Video from 'react-native-video';
-
+// import Video from 'react-native-web';
+import { Video } from 'expo-av';
 export default function App() {
+
+
   const [userInput, setUserInput] = useState('');
   const [botResponse, setBotResponse] = useState('');
   const [messages, setMessages] = useState([]);
+  const [status, setStatus] = useState([]);
   
- 
+  // const [video, setVideo] = useState(null);
+
+  const video = React.useRef(null);
+
+
+  // useEffect(() => {
+  //   async function loadVideo() {
+  //     const { status } = await Video.requestPermissionsAsync();
+  //     if (status !== 'granted') return;
+  
+  //     const videoObject = new Video.Player();
+  //     videoObject.setOnPlaybackStatusUpdate((playbackStatus) => {
+  //       if (playbackStatus.didJustFinish) {
+  //         videoObject.replayAsync();
+  //       }
+  //     });
+  //     await videoObject.loadAsync(require('./assets/videos/9878-background-full-screen.mp4'), {}, true);
+  //     setVideo(videoObject);
+  //   }
+  
+  //   loadVideo();
+  // }, []);
 
   const handlePress = () => {
     fetch(`http://127.0.0.1:5000/get?msg=${userInput}`, { mode: 'cors' })
@@ -32,11 +61,16 @@ export default function App() {
 <View style={styles.container}>
 
 
+      <Video
+        ref={video}
+        style={styles.backgroundVideo}
+        resizeMode="cover"
+        source={require("./assets/videos/background-full-screen_deOOkxTp.mp4")}
+        isLooping
+        shouldPlay
+      />
+    
 
-<video autoPlay muted loop playsInline>
-          <source src="./assets/videos/9878-background-full-screen.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
   {messages.length > 1 ? (
     <View style={styles.messagesBox}>
       <FlatList
@@ -82,18 +116,25 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  backgroundVideo: {
+    overflow: 'inherit',
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    right: 788,
+  },
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
     padding: 20,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
   },
   input: {
     height: 40,
     width: '100%',
-    borderColor: 'gray',
-    borderWidth: 1,
+    borderColor: 'white',
+    borderWidth: 3,
     marginBottom: 10,
     paddingHorizontal: 10,
   },
