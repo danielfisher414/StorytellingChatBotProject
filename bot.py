0c051fd46d9d4915c98bbcd423da456994625e9f
Daniel <47793260+danielfisher414@users.noreply.github.com>
2023-04-01 10:40:10 +0100

    added a video background to the app

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ import os
 
 app = Flask(__name__)
 
-chatbot = ChatBot('hobbit bot',
+chatbot = ChatBot('new bot',
     logic_adapters=[
         {
             'import_path': 'chatterbot.logic.BestMatch',
@@ -27,7 +27,7 @@ trainerCorpus = ChatterBotCorpusTrainer(chatbot)
 
 
 trainerCorpus.train(
-    "chatterbot.corpus.english.greetings",
+    # "chatterbot.corpus.english.greetings",
     "chatterbot.corpus.english.conversations",
     
 )
@@ -52,7 +52,31 @@ for file_name in files:
         data = file.read().splitlines()
     trainer.train(data)
     
-    
+training_data = [
+    {
+        "tag": "greeting",
+        "patterns": ["Hi", "How are you", "Is anyone there?", "Hello", "Good day"],
+        "responses": ["Hello, thanks for visiting", "Good to see you again", "Hi there, how can I help?"]
+    },
+    {
+        "tag": "goodbye",
+        "patterns": ["Bye", "See you later", "Goodbye"],
+        "responses": ["Goodbye", "Have a nice day", "See you soon"]
+    },
+        {
+        "tag": "sad",
+        "patterns": ["I'm not feeling well", "I'm quite ill and sad", "I feel sad today,","I'm not feeling very good","I dont feel very good"],
+        "responses": ["You should be stronger", "I hope you feel better soon", "Hopefully tomorrow you will feel better"]
+    }
+]
+for data in training_data:
+    patterns = data["patterns"]
+    responses = data["responses"]
+    tag = data["tag"]
+    for pattern in patterns:
+        trainer.train([pattern, tag])
+    for response in responses:
+        trainer.train([response, tag])
 # trainer.train(training_data)
 
 
@@ -69,38 +93,38 @@ for file_name in files:
 # def home():
 #     return render_template('index.html')
 
-app = Flask(__name__)
-cors = CORS(app)
+# app = Flask(__name__)
+# cors = CORS(app)
 
-@app.route('/get')
-@cross_origin()
-def get_bot_response():
-    user_input = request.args.get('msg')
-    bot_response = str(chatbot.get_response(user_input))
-    # print(bot_response)
-    if (str(bot_response).startswith("- ")):
-      bot_response = str(bot_response)[2:]
-    return jsonify({'response':bot_response})
+# @app.route('/get')
+# @cross_origin()
+# def get_bot_response():
+#     user_input = request.args.get('msg')
+#     bot_response = str(chatbot.get_response(user_input))
+#     # print(bot_response)
+#     if (str(bot_response).startswith("- ")):
+#       bot_response = str(bot_response)[2:]
+#     return jsonify({'response':bot_response})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
 # end flash application
-# while True:
-#     try:
-#         user_input = input()
-#         bot_response = chatbot.get_response(user_input)
-#         # if bot_response.text.startswith()
-#         if (str(bot_response).startswith("- ")):
-#             bot_response = str(bot_response)[2:]
-#         print("Hobbiton: ",bot_response)
+while True:
+    try:
+        user_input = input()
+        bot_response = chatbot.get_response(user_input)
+        # if bot_response.text.startswith()
+        if (str(bot_response).startswith("- ")):
+            bot_response = str(bot_response)[2:]
+        print("Hobbiton: ",bot_response)
         
-#         feedback=input('was that helpful?')
-#         if(feedback.lower() in ['no','n','nope']):
-#             statement = Statement(text=bot_response)
-#             statement.in_response_to = user_input
-#             chatbot.learn_response(statement, statement)
-#             print("Thank you for teaching me.")
+        feedback=input('was that helpful?')
+        if(feedback.lower() in ['no','n','nope']):
+            statement = Statement(text=bot_response)
+            statement.in_response_to = user_input
+            chatbot.learn_response(statement, statement)
+            print("Thank you for teaching me.")
 
-#     except KeyboardInterrupt:
-#         break
+    except KeyboardInterrupt:
+        break
--- front-end/storytellingApp/App.js
+++ front-end/storytellingApp/App.js
@@ -1,5 +1,5 @@
 import React, { useState,Component,useEffect,useRef } from 'react';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View, FlatList   } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View, FlatList,Dimensions,ViewStyle     } from 'react-native';
 // import {Video} from 'expo-av';
 // import { requireNativeComponent } from 'react-native';
 // import { Platform } from 'react-native';
@@ -8,8 +8,25 @@ import { StyleSheet, Text, TextInput, TouchableOpacity, View, FlatList   } from 
 // import { Image } from 'react-native-web';
 // import Video from 'react-native-web';
 import { Video } from 'expo-av';
+
+// const videoStyle: ViewStyle = {
+//   width: 200,
+//   height: 200,
+//   alignSelf: 'center',
+//   marginTop: 20,
+//   borderRadius: 10,
+// };
+
 export default function App() {
 
+  // const { width, height } = Dimensions.get('window');
+
+  // const styles = StyleSheet.create({
+  //   backgroundVideo: {
+  //     width: width * 0.8,
+  //     height: height * 0.5,
+  //   },
+  // });
 
   const [userInput, setUserInput] = useState('');
   const [botResponse, setBotResponse] = useState('');
@@ -18,7 +35,8 @@ export default function App() {
   
   // const [video, setVideo] = useState(null);
 
-  const video = React.useRef(null);
+  const video = useRef(null);
+
 
 
   // useEffect(() => {
@@ -60,17 +78,19 @@ export default function App() {
   return (
 <View style={styles.container}>
 
-
+{/* <View style={{position:'absolute',width:'100%', height:'100%',}}> */}
       <Video
         ref={video}
-        style={styles.backgroundVideo}
+        style={styles.video}
         resizeMode="cover"
-        source={require("./assets/videos/background-full-screen_deOOkxTp.mp4")}
+        source={require("./assets/videos/bilbo_House.mp4")}
         isLooping
         shouldPlay
+        styleProp
+        
       />
     
-
+    {/* </View> */}
   {messages.length > 1 ? (
     <View style={styles.messagesBox}>
       <FlatList
@@ -116,27 +136,36 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  backgroundVideo: {
-    overflow: 'inherit',
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-    right: 788,
-  },
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    padding: 20,
+    // padding: 20,
     backgroundColor: 'red',
+  },
+  video: {
+    position:'absolute',
+    overflow: 'hidden', /* Hide vertical scrollbar */
+    // overflow-x: 'hidden', /* Hide horizontal scrollbar *
+    color:'blue',
+    width: '100%',
+    height: '100%',
+    // transform:[{translateX:'-50%'}]
+    // position: 'absolute',
+    // width: '100%',
+    // height: '100%',
+    // right: 720,
+    // resizeMode:'cover'
   },
   input: {
     height: 40,
     width: '100%',
     borderColor: 'white',
+    color:'white',
     borderWidth: 3,
     marginBottom: 10,
     paddingHorizontal: 10,
+    fontWeight:'bold',
   },
   button: {
     backgroundColor: 'blue',
